dc8d03cf3a52b48273c43ad79f4ba2e15e9a6d2b
munazzabegam <github.com/munazzabegam>
2025-04-17 13:36:52 +0530

    updated

--- components/navbar.php
+++ components/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-dark fixed-top bg-transparent" id="main-navbar">
   <div class="container-fluid">
-    <button class="btn btn-outline-light" id="collapseBtn">
+    <button class="btn btn-warning" id="collapseBtn">
       <i class="bi bi-list"></i>
     </button>
     <a class="navbar-brand ms-3" href="#">Café <span class="text-warning">Bliss</span></a>
 
     <!-- Dark/Light Mode Toggle Button -->
-    <button class="btn btn-outline-light ms-auto" id="themeToggle">
+    <button class="btn btn-warning ms-auto" id="themeToggle">
       <i class="bi bi-moon-fill" id="themeIcon"></i>
     </button>
   </div>
